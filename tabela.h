d8d5ba3a4885112256120deb78e59eec71e63477
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2023-12-04 09:46:27 -0300

    tabela copypaste

--- tabela.h
+++ tabela.h
@@ -1,11 +1,26 @@
 #ifndef TABELA_H
 #define TABELA_H
 
+#include "QTableWidget"
+#include "QHeaderView"
+#include "grafo.h"
 
 class Tabela
 {
+private:
+    QTableWidget *tabela;
+    Grafo<int> *grafo;
+
 public:
     Tabela();
+    Tabela(QTableWidget *parent, const int &tamanho);
+    ~Tabela();
+    void start();
+    void limpar();
+    void atualizar();
+    void inserirAresta(const int &vertice1, const int& vertice2, const int &peso);
+    void alterarAresta(const int &vertice1, const int& vertice2, const int &peso);
+    void removerAresta(const int &vertice1, const int& vertice2);
 };
 
 #endif // TABELA_H
