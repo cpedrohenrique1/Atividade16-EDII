e17396d95aa956fe14a67b68e0a77e601d0d5236
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2023-12-06 22:16:33 -0300

    definicao etiqueta functions

--- etiqueta.h
+++ etiqueta.h
@@ -7,7 +7,7 @@ class Etiqueta
 private:
     TYPE CustoAcumulado;
     int VerticePrecedente;
-    int QuantidadeArestasVisitavas;
+    int QuantidadeArestasVisitadas;
     bool SituacaoVertice;
 public:
     Etiqueta(TYPE valorInicial = TYPE()):   CustoAcumulado(valorInicial),
@@ -15,6 +15,14 @@ public:
                                             QuantidadeArestasVisitavas(0),
                                             SituacaoVertice(false)
     {}
+    void setCustoAcumulado(TYPE custo_acumulado);
+    TYPE getCustoAcumulado()const;
+    void setVerticePrecedente(int vertice_precedente);
+    int getVerticePrecedente()const;
+    void setQuantidadeArestasVisitadas(int quantidade_arestas_visitadas);
+    int getQuantidadeArestasVisitadas() const;
+    void setSituacaoVertice(bool situacao_vertice);
+    bool getSituacaoVertice() const;
 };
 
 #endif // ETIQUETA_H
