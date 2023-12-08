2e91a466aa1e8f4a97616c0425c46dbb4dd317c6
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2023-12-08 00:38:53 -0300

    Refactor Caminho class constructor and methods

--- caminho.h
+++ caminho.h
@@ -8,52 +8,59 @@ class Caminho
 {
 private:
     Grafo<TYPE>* grafo;
-    Lista<Etiqueta<TYPE>>** etiquetas;
+    Lista<Etiqueta<TYPE>>** etiqueta;
+    int n_vertices;
 public:
-    Caminho(const Grafo<TYPE>* grafo): grafo(grafo){
+    Caminho(const Grafo<TYPE>* grafo):  grafo(grafo),
+                                        etiqueta(0),
+                                        n_vertices(0)
+    {
+        if (!grafo){
+            throw QString("Grafo nao inicializado");
+        }
         try{
-            etiquetas = new Lista<Etiqueta<TYPE>>*[grafo->getNVertices()];
-            for(int i = 0; i < grafo->getNVertices(); ++i){
-                etiquetas[i] = new Lista<Etiqueta<TYPE>>;
+            n_vertices = grafo->getNVertices();
+            etiqueta = new Lista<Etiqueta<TYPE>>*[n_vertices];
+            for(int i = 0; i < n_vertices; ++i){
+                etiqueta[i] = new Lista<Etiqueta<TYPE>>;
             }
         }catch(std::bad_alloc& e){
             throw QString("Erro ao alocar memoria");
         }
     }
-    void desenharCaminho(const int& vertice1, const int& vertice2){
+    void encontrarCaminho(const int& vertice1, const int& vertice2){
         if (!grafo){
             throw QString("Grafo nao inicializado");
         }
-        if (!etiquetas){
-            throw QString("Lista de etiquetas nao inicializada");
+        if (!etiqueta){
+            throw QString("Lista de etiqueta nao inicializada");
         }
-        if ((vertice1 < 0 || vertice1 >= grafo->getNVertices()) || (vertice2 < 0 || vertice2 >= grafo->getNVertices())){
+        if ((vertice1 < 0 || vertice1 >= n_vertices) || (vertice2 < 0 || vertice2 >= n_vertices)){
             throw QString("Vertice invalido");
         }
         if (vertice1 == vertice2){
-            etiquetas[vertice1 - 1]->inserirInicio(Etiqueta<TYPE>(0,0,0,true));
+            etiqueta[vertice1 - 1]->inserirInicio(Etiqueta<TYPE>(TYPE(),0,0,true));
             return;
         }
-        etiquetas[vertice1 - 1]->inserirInicio(Etiqueta<TYPE>(0,0,0,true));
+        etiqueta[vertice1 - 1]->inserirInicio(Etiqueta<TYPE>(TYPE(),0,0,true));
         for (int i = 0; i < grafo->getTamanhoLista(vertice1 - 1); ++i){
             if (grafo->getTamanhoLista(vertice1 - 1) == 1){
-                etiquetas[grafo->getNOGrafo(vertice1, i).getVertice()]->inserirInicio(Etiqueta<TYPE>(0,0,0,true));
+                NOGrafo<TYPE> temp = grafo->getNOGrafo(vertice1 - 1, i);
+                etiqueta[temp.getVertice()]->inserirInicio(Etiqueta<TYPE>(temp.getPeso(),vertice1,,true));
             }else{
-                Etiqueta<TYPE> temp;
             }
-
         }
     }
     ~Caminho(){
-        if (etiquetas){
-            for (int i = 0; i < grafo->getNVertices(); ++i){
-                if (etiquetas[i]){
-                    delete etiquetas[i];
-                    etiquetas[i] = 0;
+        if (etiqueta){
+            for (int i = 0; i < n_vertices; ++i){
+                if (etiqueta[i]){
+                    delete etiqueta[i];
+                    etiqueta[i] = 0;
                 }
             }
-            delete[] etiquetas;
-            etiquetas = 0;
+            delete[] etiqueta;
+            etiqueta = 0;
         }
     }
 };
